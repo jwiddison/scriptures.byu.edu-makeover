679d11946ca43689d595d69790d92237098e12bd
Jordan Widdison <jordan.widdison@gmail.com>
2017-02-02 17:19:24 -0700

    Added search in a modal that triggers onClick of icon in navbar.

---
+++ scripts/loadModal.js
@@ -0,0 +1,6 @@
+// $("#searchModal").on("show.bs.modal", function(e) {
+//     console.log("Inside Function");
+//     var link = $(e.relatedTarget);
+//     $(this).find(".modal-body").load(link.attr("href"));
+// });
+// console.log("Outside Function");
